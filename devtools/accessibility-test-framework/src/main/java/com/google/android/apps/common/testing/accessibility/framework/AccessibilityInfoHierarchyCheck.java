31661510006b40e97b7b6e85def75a149064f930
pweaver <pweaver@google.com>
2015-05-08 21:08:03 +0000

    Version 2.0

--- devtools/accessibility-test-framework/src/main/java/com/google/android/apps/common/testing/accessibility/framework/AccessibilityInfoHierarchyCheck.java
+++ devtools/accessibility-test-framework/src/main/java/com/google/android/apps/common/testing/accessibility/framework/AccessibilityInfoHierarchyCheck.java
@@ -16,7 +16,13 @@
 
 package com.google.android.apps.common.testing.accessibility.framework;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.googlecode.eyesfree.utils.AccessibilityNodeInfoUtils;
+import com.googlecode.eyesfree.utils.NodeFilter;
 
 import java.util.List;
 
@@ -24,16 +30,42 @@ import java.util.List;
  * Base class to check the accessibility of all {@code Info}s in a hierarchy.
  */
 public abstract class AccessibilityInfoHierarchyCheck extends AccessibilityCheck {
+  private static final NodeFilter WIDE_OPEN_FILTER = new NodeFilter() {
+    @Override
+    public boolean accept(Context context, AccessibilityNodeInfoCompat node) {
+      return true;
+    }
+  };
+
   public AccessibilityInfoHierarchyCheck() {
   }
 
   /**
    * Run the check on the info.
-   * @param root The root of the AccessibilityNodeInfo hierarchy to check.
+   *
+   * @param root The root of the {@link AccessibilityNodeInfo} hierarchy to check.
+   * @param context The context of the service.
+   * @param metadata An optional {@link Bundle} that may contain check metadata defined by
+   *        {@link AccessibilityCheckMetadata}.
    * @return A list of interesting results encountered while running the check. The list will be
-   * empty if the check passes without incident.
+   *         empty if the check passes without incident.
    */
   public abstract List<AccessibilityInfoCheckResult> runCheckOnInfoHierarchy(
-      AccessibilityNodeInfo root);
+      AccessibilityNodeInfo root, Context context, Bundle metadata);
 
+  /**
+   * @see AccessibilityInfoHierarchyCheck#runCheckOnInfoHierarchy(AccessibilityNodeInfo, Context,
+   *      Bundle)
+   */
+  public List<AccessibilityInfoCheckResult> runCheckOnInfoHierarchy(
+      AccessibilityNodeInfo root, Context context) {
+    return runCheckOnInfoHierarchy(root, context, null);
+  }
+
+  static List<AccessibilityNodeInfoCompat> getAllInfoCompatsInHierarchy(Context context,
+      AccessibilityNodeInfo root) {
+    return AccessibilityNodeInfoUtils.searchAllFromBfs(context,
+        new AccessibilityNodeInfoCompat(root),
+        AccessibilityInfoHierarchyCheck.WIDE_OPEN_FILTER);
+  }
 }
